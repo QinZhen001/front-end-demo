743e5cde8952ac0ba67817d8b8838f4b81ec3439
渣臻 <qz519189636@qq.com>
2018-03-30 15:49:49 +0800

    qz commit

--- vue-analysis/data-bind.js
+++ vue-analysis/data-bind.js
@@ -228,7 +228,72 @@ export const arrayMethods = Object.create(arrayProto)
         // cache original method
         /*将数组的原生方法缓存起来，后面要调用*/
         const original = arrayProto[method]
+        def(arrayMethods, method, function mutator() {
+            // avoid leaking arguments:
+            // http://jsperf.com/closure-with-arguments
+            let i = arguments.length
+            const args = new Array(i)
+            while (i--) {
+                args[i] = arguments[i]
+            }
+            //调用原生的数组方法
+            const result = original.apply(this, args)
+
+            //数组插入的元素需要重新进行observe才能响应
+            const ob = this.__ob__
+            let inserted
+            switch (method) {
+                case 'push':
+                    inserted = args
+                    break
+                case 'unshift':
+                    inserted = args
+                    break
+                case 'splice':
+                    inserted = args.slice(2)
+                    break
+            }
+            if (inserted) {
+                ob.observeArray(inserted)
+            }
+
+            // notify change
+            /*dep通知所有注册的观察者进行响应式处理*/
+            ob.dep.notify()
+            return result
+        })
     })
+// 在保证不污染不覆盖数组原生方法添加监听，主要做了两个操作，
+// 第一是通知所有注册的观察者进行响应式处理，
+// 第二是如果是添加成员的操作，需要对新成员进行observe。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- vue-analysis/util.js
+++ vue-analysis/util.js
@@ -12,3 +12,49 @@ export function def(obj: Object, key: string, val: any, enumerable
         configurable: true
     })
 }
+
+
+/**
+ * Define a reactive property on an Object.
+ */
+export function defineReactive
+(obj: Object,
+ key: string,
+ val: any,
+ customSetter: Function) {
+    //在闭包中定义一个dep对象
+    const dep = new Dep()
+
+    const property = Object.getOwnPropertyDescriptor(obj, key)
+    if (property && property.configurable === false) {
+        return
+    }
+    /*如果之前该对象已经预设了getter以及setter函数则将其取出来，
+     新定义的getter/setter中会将其执行，
+     保证不会覆盖之前已经定义的getter/setter。
+     */
+    // cater for pre-defined getter/setters
+    const getter = property && property.get
+    const setter = property && property.set
+
+    //对象的子对象递归进行observe并返回子节点的observer对象
+    let childOb = observe(val)
+    Object.defineProperty(obj, key, {
+        enumerable: true,
+        configurable: true,
+        get: function reactiveGetter() {
+            //如果原本对象拥有getter方法则执行
+            const value = getter ? getter.call(obj) : val
+            if (Dep.target) {
+                //进行依赖收集
+                dep.depend()
+                if (childOb) {
+                    // 子对象进行依赖收集，其实就是将同一个watcher观察者实例放进两个depend中
+                    // 一个是正在本身闭包中的depend，另一个是子元素的depend
+                }
+            }
+        }
+    })
+
+
+}
