7f06f07b84e0c449755257b729153fd93b1f8c97
渣臻 <qz519189636@qq.com>
2018-03-31 22:33:09 +0800

    qz commit

---
+++ vue-analysis/VNode.js
@@ -0,0 +1,54 @@
+//VNode节点
+//https://github.com/answershuto/learnVue/blob/master/docs/VNode%E8%8A%82%E7%82%B9.MarkDown
+
+export default class VNode {
+    tag: string | void;
+    data: VNodeData | void;
+    children: Array<VNode>;
+    text: string | void;
+    elm: Node | void
+    ns: string | void
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
