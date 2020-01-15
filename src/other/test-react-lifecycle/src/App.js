053bb6b606fd299b9fac3ddadd7bd682df7f29b6
秦臻 <qz519189636@qq.com>
2020-01-15 15:03:58 +0800

    qz commit

---
+++ src/other/test-react-lifecycle/src/App.js
@@ -0,0 +1,101 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            boss: "我自己"
+        }
+        this.changeBoss = this.changeBoss.bind(this)
+    }
+
+    changeBoss() {
+        this.setState({
+            boss: this.state.boss + "1"
+        })
+    }
+
+  /**
+   *
+   * @returns {*}
+   */
+  render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <br/>
+                <button onClick={this.changeBoss}>改变老大</button>
+                <br/>
+                <TestComponent boss={this.state.boss}/>
+            </div>
+        );
+    }
+}
+
+
+class TestComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            soldiers: ["小虎", "小凤", "小凰"]
+        }
+        console.log("组件初始化")
+        this.addSoldier = this.addSoldier.bind(this)
+    }
+
+    componentWillMount() {
+        console.log("组件马上就要挂载了")
+    }
+
+    componentDidMount() {
+        console.log("组件已经挂载了")
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log("组件要接收父组件的值了 ", nextProps)
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("判断是不是要更新组件")
+        return true    // 记得要返回true(这里可以进行条件判断)
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log("组件马上就要更新了")
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("组件更新完毕")
+    }
+
+    componentWillUnmount() {
+        console.log("组件马上就要卸载了")
+    }
+
+    addSoldier() {
+        this.setState({
+            soldiers: [...this.state.soldiers, "新兵蛋子" + Math.random()]
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.props.boss}</h2>
+                <button onClick={this.addSoldier}>新兵入伍</button>
+                <ul>
+                    {this.state.soldiers.map(item => {
+                        return <li key={item}>{item}</li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/pages/index/index.js
@@ -0,0 +1,35 @@
+// 首页
+
+import "./index.html"
+
+
+// url拼接上.html
+const contents = [
+  {
+    title: "cross-domain 跨域通信",
+    url: "cross-domain"
+  }
+]
+
+
+const body = document.body
+// console.log("body", body)
+
+const ul = document.createElement("ul")
+contents.sort((a, b) => {
+  const titleA = a.title
+  const titleB = b.title
+  return titleA.charCodeAt(0) - titleB.charCodeAt(0)
+})
+contents.forEach(item => {
+  const title = item.title
+  const url = title.split(" ")[0] + ".html"
+  const li = document.createElement("li")
+  const a = document.createElement("a")
+  a.innerText = title
+  a.setAttribute("href", url)
+  li.appendChild(a)
+  ul.appendChild(li)
+})
+
+body.appendChild(ul)
