c27d7c32017f3b6d9a2fde7194e31d4c96529b0f
qinzhen <qz519189636@qq.com>
2021-08-30 16:24:17 +0800

    feat: web worker

--- other/underscore/index.js
+++ other/underscore/index.js
@@ -1,4 +1,7 @@
 // https://juejin.cn/post/6844903908972691469
+// https://juejin.cn/post/6844903511394615303
+
+const { push } = require("core-js/fn/array");
 
 var _ = function (obj) {
   if (obj instanceof _) {
@@ -19,10 +22,11 @@ _.chain = function (obj) {
 };
 
 var chainResult = function (instance, obj) {
-  // 如果实例中有_chain 为 true 这个属性，则返回实例 
-  // 支持链式调用的实例对象  
+  // 如果实例中有_chain 为 true 这个属性，则返回实例
+  // 支持链式调用的实例对象
   // { _chain: true, this._wrapped: [3, 2, 1] }，否则直接返回这个对象[3, 2, 1]。
-  return instance._chain ? _(obj).chain() : obj
+  return instance._chain ? _(obj).chain() : obj;
+  // chain() 是 mixin的时候挂载上去的
 };
 
 var ArrayProto = Array.prototype;
@@ -37,3 +41,49 @@ _.each(["pop", "push", "reverse", "shift", "sort", "splice", "unshift"], functio
     return chainResult(this, obj);
   };
 });
+
+// value
+_.prototype.value = function () {
+  return this._wrapped;
+};
+
+// 顺便提供了几个别名
+_.prototype.valueOf = _.prototype.toJSON = _.prototype.value;
+
+// toString
+_.prototype.toString = function () {
+  return String(this._wrapped);
+};
+
+// mixin
+_.mixin = function (obj) {
+  _.each(_.functions(obj), function (name) {
+    var func = (_[name] = obj[name]);
+
+    _.prototype[name] = function () {
+      var args = [this._wrapped];
+      push.apply(args, arguments);
+      return chainResult(this, func.apply(_, args));
+    };
+  });
+
+  return _;
+};
+
+_.mixin(_);
+
+// _mixin(_) 把静态方法挂载到了_.prototype上，
+// 也就是_.prototype.chain方法 也就是 _.chain方法。
+
+
+
+
+
+// noConflict  防冲突函数
+
+// 暂存在 root 上， 执行noConflict时再赋值回来
+var previousUnderscore = root._;
+_.noConflict = function(){
+  root._ = previousUnderscore
+  return this 
+}
