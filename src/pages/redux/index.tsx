e1ac6e3b41ec96dfbba96552308ad3308b87b77a
qinzhen <qz519189636@qq.com>
2022-04-29 16:55:49 +0800

    feat: change redux demo

--- src/pages/redux/index.tsx
+++ src/pages/redux/index.tsx
@@ -1,3 +1,19 @@
+// @ts-nocheck
+import { TestRedux } from "./components/test-redux";
+import { createStore, applyMiddleware } from "./mini-redux/custom-redux";
+import { counterReducer } from "./reducer/index";
+import { Provider } from "./mini-redux/custom-react-redux";
+import thunk from "./mini-redux/custom-redux-thunk";
+import arrayThunk from "./mini-redux/custom-redux-array";
+
+let store = createStore(counterReducer, applyMiddleware(thunk, arrayThunk));
+
 export const ReduxPageComponent = () => {
-  return <div>ReduxPageComponent</div>;
+  return (
+    <div>
+      <Provider store={store}>
+        <TestRedux></TestRedux>
+      </Provider>
+    </div>
+  );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import process from "process";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 const { name = "" } = require("./package.json");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? `/${name}/` : "",
-  plugins: [react()],
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+    react({
+      babel: {
+        parserOpts: {
+          plugins: ["decorators-legacy"],
+        },
+      },
+    }),
+  ],
 });
