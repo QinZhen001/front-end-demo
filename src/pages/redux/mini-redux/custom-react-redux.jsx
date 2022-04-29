7200438009eeb16353a6c28ad37d4ec1ffa1be6d
qinzhen <qz519189636@qq.com>
2022-04-29 16:55:53 +0800

    Update custom-react-redux.jsx

--- src/pages/redux/mini-redux/custom-react-redux.jsx
+++ src/pages/redux/mini-redux/custom-react-redux.jsx
@@ -5,6 +5,7 @@ import { bindActionCreators } from "./custom-redux";
 // connect 负责链接组件，把redux里的数据放到组件的属性里
 // 1.负责接收一个组件，把state里的一些数据放进去，放回一个组件
 // 2.数据变化的时候，能够通知组件
+// state => state
 // 相当于 function(state){
 //     return state
 // }
