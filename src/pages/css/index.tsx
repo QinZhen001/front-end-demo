25e794792f5c0f039a4b1960d8539613b8806729
qinzhen <qz519189636@qq.com>
2023-04-06 16:30:34 +0800

    feat: editor

--- src/pages/css/index.tsx
+++ src/pages/css/index.tsx
@@ -2,12 +2,24 @@ import { lazy } from "react"
 import { CommonPageRouter } from "../default"
 
 const Progress = lazy(() => import('./progress'));
+const Lyric = lazy(() => import('./lyric'));
+const AtEditor = lazy(() => import('./at-editor'));
 
 export const children = [
   {
     path: "progress",
     element: <Progress></Progress>,
     title: "带文字过渡效果的进度条",
+  },
+  {
+    path: "lyric",
+    element: <Lyric></Lyric>,
+    title: "歌词渐变过渡效果",
+  },
+  {
+    path: "at-editor",
+    element: <AtEditor></AtEditor>,
+    title: "editor @人功能",
   },
 ]
 
