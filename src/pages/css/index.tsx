e640200e03f72aad68a02a7099df3d183346f3b0
qinzhen <qz519189636@qq.com>
2023-05-15 17:00:40 +0800

    feat: chinese input

--- src/pages/css/index.tsx
+++ src/pages/css/index.tsx
@@ -1,9 +1,12 @@
 import { lazy } from "react"
 import { CommonPageRouter } from "../default"
+import ChineseInput from "./chinese-input "
 
 const Progress = lazy(() => import('./progress'));
 const Lyric = lazy(() => import('./lyric'));
 const AtEditor = lazy(() => import('./at-editor'));
+// const ChineseInput from "" = lazy(() => import("./chineseinput/rrr.tsx"));
+// import ChineseInput from "./chineseinput/rrr.tsx"
 
 export const children = [
   {
@@ -20,6 +23,11 @@ export const children = [
     path: "at-editor",
     element: <AtEditor></AtEditor>,
     title: "editor @人功能",
+  },
+  {
+    path: "chinese-input",
+    element: <ChineseInput></ChineseInput>,
+    title: "监听中文输入法事件",
   },
 ]
 
