7473c8684e5a57852df42a1ec95c25561f0f2d4d
qinzhen <qz519189636@qq.com>
2023-05-15 17:56:47 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,19 @@ import legacy from "@vitejs/plugin-legacy";
 
 const { name = "" } = require("./package.json");
 
+console.log("process.env.VERCEL_ENV", process.env.VERCEL_ENV)
+console.log("process.env.VITE_VERCEL_ENV", process.env.VITE_VERCEL_ENV)
+
+let base = ""
+if (process.env.CI && process.env.VERCEL_ENV == "production") {
+  base = ""
+} else if (process.env.NODE_ENV == "production") {
+  base = `/${name}/`
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? `/${name}/` : "",
+  base: base,
   plugins: [
     legacy({
       targets: ["defaults", "not IE 11"],
