7badcd9f9473914230c95481246f72dcb22efe35
qinzhen <qz519189636@qq.com>
2023-12-21 21:15:49 +0800

    feat: use yarn

--- src/pages/vue/simple-vue-router/index.tsx
+++ src/pages/vue/simple-vue-router/index.tsx
@@ -1,4 +1,30 @@
+import Vue from "./assets/vue.min.js"
+import { useEffect } from "react";
+import VueRouter from "./myVueRouter.js"
+
 const SimpleVueRouter = () => {
+
+  useEffect(() => { 
+    init()
+  },[])
+
+  const init = () => {
+    const vue = new Vue({
+      el: '#app',
+      data: {
+        name: "test",
+        age: 12,
+        html: '<button>这是一个按钮</button>'
+      },
+      created() {
+      },
+      methods: {
+
+      }
+    })
+    vue.use(VueRouter)
+  }
+
   return <div>simple-vue-router</div>;
 }
 
