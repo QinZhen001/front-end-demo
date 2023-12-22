ae48fc1ffd54fcc50e5aeeb30f521b15a8041715
qinzhen <qz519189636@qq.com>
2023-12-22 20:12:46 +0800

    feat: vue

--- src/pages/vuex/store/index.js
+++ src/pages/vuex/store/index.js
@@ -1,29 +1,29 @@
 // https://github.com/Sunny-lucking/howToBuildMyVuex/blob/master/src/store/index.js
 
-import Vue from "vue";
-import Vuex from "../simple-vuex/myVuex";
+// import Vue from "vue";
+// import Vuex from "../simple-vuex/myVuex";
 
-Vue.use(Vuex);
+// Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    num: 0,
-  },
-  getters: {
-    getNum(state) {
-      return state.num + 1;
-    },
-  },
-  mutations: {
-    incre(state, arg) {
-      state.num += arg;
-    },
-  },
-  actions: {
-    actionTest({ commit }, params) {
-      setTimeout(() => {
-        commit("incre", params);
-      }, 2000);
-    },
-  },
-});
+// export default new Vuex.Store({
+//   state: {
+//     num: 0,
+//   },
+//   getters: {
+//     getNum(state) {
+//       return state.num + 1;
+//     },
+//   },
+//   mutations: {
+//     incre(state, arg) {
+//       state.num += arg;
+//     },
+//   },
+//   actions: {
+//     actionTest({ commit }, params) {
+//       setTimeout(() => {
+//         commit("incre", params);
+//       }, 2000);
+//     },
+//   },
+// });
