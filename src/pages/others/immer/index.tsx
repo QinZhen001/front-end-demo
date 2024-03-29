17e31ba1e613881f9de8ae2ad790d65bd645d686
qinzhen <qz519189636@qq.com>
2024-03-29 19:53:48 +0800

    feat: update

--- src/pages/others/immer/index.tsx
+++ src/pages/others/immer/index.tsx
@@ -1,28 +1,38 @@
-import { useState } from 'react'
-import { Store } from './src'
+import { useState } from "react"
+import { Store } from "./src"
 
-let store = new Store({ name: 'jack', age: 18,content:{text} data: { aaa: "aaa", bbb: "bbb" } })
+const store = new Store({
+  name: "jack",
+  age: 18,
+  content: { text: "asfvasfafsa" },
+  data: { aaa: "aaa", bbb: "bbb" },
+})
 
 const Immer = () => {
   const [data, setData] = useState(store.get("data"))
 
   const onClickChange = () => {
-    const oldData = store.get("data") 
+    const oldData = store.get("data")
+    const oldContent = store.get("content")
     store.set("data", {
-      ddd: "ddd"
+      ddd: "ddd",
     })
     const newData = store.get("data")
+    const newContent = store.get("content")
     console.log("oldData == newData", oldData === newData)
+    console.log("oldContent == newContent", oldContent === newContent)
     setData(store.get("data"))
   }
 
-  return <div>
-    <div>value:</div>
-    <pre>{JSON.stringify(data, null, 2)}</pre>
+  return (
     <div>
-      <button onClick={onClickChange}>change</button>
+      <div>value:</div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div>
+        <button onClick={onClickChange}>change</button>
+      </div>
     </div>
-  </div>
+  )
 }
 
-export default Immer;
+export default Immer
--- src/pages/others/immer/src/index.ts
+++ src/pages/others/immer/src/index.ts
@@ -1,6 +1,10 @@
 // immer.js:也许更适合你的immutable js库
 // https://juejin.cn/post/6844904111402385422#heading-6
 
+// https://immerjs.github.io/immer/zh-CN/
+// 会将所有更改应用到临时 draft，它是 currentState 的代理。一旦你完成了所有的 mutations，
+// Immer 将根据对 draft state 的 mutations 生成 nextState
+
 export class Store {
   modified: boolean
   source: any
@@ -37,4 +41,32 @@ export class Store {
   }
 }
 
-function produce()
+const PROXY_FLAG = "@@SYMBOL_PROXY_FLAG"
+const handler = {
+  get(target: any, key: any) {
+    // 如果遇到了这个 flag 我们直接返回我们操作的 target
+    if (key === PROXY_FLAG) return target
+    return target.get(key)
+  },
+  set(target: any, key: any, value: any) {
+    return target.set(key, value)
+  },
+}
+
+function produce(state: any, producer: (state: any) => {}) {
+  const store = new Store(state)
+  const proxy = new Proxy(store, handler)
+
+  // 执行我们传入的 producer 函数，我们实际操作的都是 proxy 实例，
+  // 所有有副作用的操作都会在 proxy 内部进行判断，是否最终要对 store 进行改动。
+  producer(proxy)
+
+  // 处理完成之后，通过 flag 拿到 store 实例
+  const newState = proxy[PROXY_FLAG]
+
+  if (newState.modified) {
+    return newState.copy
+  }
+
+  return newState.source
+}
