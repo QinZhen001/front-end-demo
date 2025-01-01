faa854e4245ab103840f42a281723db2d72b9be2
渣臻 <qz519189636@qq.com>
2025-01-01 13:52:42 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: "/front-end-demo/",
+  basePath: "/front-end-demo",
   reactStrictMode: false,
   output: "export",
   // output: "standalone",
